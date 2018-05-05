db19514c17b152904077537bda018b10b741fb9a
liyong <liyong@xnw.com>
2018-05-05 20:38:58 +0800

    添加组件文件

---
+++ LYCatagoryLib/Classes/NSString+GetLimitRangeString.h
@@ -0,0 +1,13 @@
+//
+//  NSString+GetLimitRangeString.h
+//  Sophists
+//
+//  Created by 勇 李 on 2018/4/25.
+//  Copyright © 2018 勇 李. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (GetLimitRangeString)
+- (NSString*)getLimitRangeString:(NSRange)range;
+@end
---
+++ LYCatagoryLib/Classes/NSString+GetLimitRangeString.m
@@ -0,0 +1,18 @@
+//
+//  NSString+GetLimitRangeString.m
+//  Sophists
+//
+//  Created by 勇 李 on 2018/4/25.
+//  Copyright © 2018 勇 李. All rights reserved.
+//
+
+#import "NSString+GetLimitRangeString.h"
+
+@implementation NSString (GetLimitRangeString)
+- (NSString*)getLimitRangeString:(NSRange)range{
+    return [self substringWithRange:range];
+//    if (range.length < self.length && range.length-range.location) {
+//        <#statements#>
+//    }
+}
+@end
